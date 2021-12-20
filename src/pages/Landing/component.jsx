aee2332e16dcf0ae9c2563bb88058e231ed970dd
Ian <iankwanhou@gmail.com>
2021-12-19 17:47:20 -0700

    json for homepage

--- src/pages/Landing/component.jsx
+++ src/pages/Landing/component.jsx
@@ -3,6 +3,7 @@ import {
   Link,
 } from 'react-router-dom';
 import Loading from '../Loading';
+import { data } from './data.json';
 import './styles.scss';
 
 export default () => {
@@ -15,7 +16,7 @@ export default () => {
   const Landing = () => (
     <>
       <div className="content">
-        <div className="quote">THERE IS ALWAYS SOMETHING NEW TO LEARN.</div>
+        <div className="quote">{data.quote}</div>
         <div>
           <img className="xueImage" src="/images/xue.png" alt="xue wu zhi jing" />
         </div>
@@ -29,21 +30,13 @@ export default () => {
             </div>
 
             <div className="profile-text">
-              <h1>Hello World.</h1>
+              <h1>{data.title}</h1>
               <p>
-                I&apos;m Ian, a senior at Dartmouth College studying computer science and Chinese.
-                I currently work part-time as a software engineer at Requipper, a startup that
-                helps outdoorsy people buy and sell used gear. At school, I am an SMP cadet in
-                Dartmouth ROTC, serving with the 424th EN CO. Learn more about my career here:
+                {data.career}
               </p>
               <Link to="/professional" className="nav-link-button">Professional</Link>
               <p>
-                I love food, exercise, and the outdoors. At Dartmouth, I lead organizations within
-                the Dartmouth Outdoors Club such as People of Color Outdoors and Bait and Bullet.
-                In the Ledyard Canoe Club, I am a flatwater trip leader and lead canoe trips
-                anywhere from down the Connecticut River to Everglades National Park!
-                You can often find me cooking hotpot in a common room on campus.
-                Learn more about my personal life here:
+                {data.personal}
               </p>
               <Link to="/personality" className="nav-link-button">Personal</Link>
             </div>
