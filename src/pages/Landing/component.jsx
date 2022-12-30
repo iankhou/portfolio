4d6018c0c154142639a2ac6526c951cca11cb209
Ian <iankwanhou@gmail.com>
2022-12-30 13:29:17 -0500

    mobile friendly adjustments

--- src/pages/Landing/component.jsx
+++ src/pages/Landing/component.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import Loading from "../Loading";
 import ScrollAnimation from "react-animate-on-scroll";
@@ -14,77 +14,85 @@ const Landing = () => {
     setIsLoading(false);
   }, []);
 
-  const LandingComponent = () => (
-    <>
-      <video autoPlay muted loop id="nycVideo">
-        <source src="videos/nyc.mp4" type="video/mp4" />
-      </video>
-      <div className="content">
-        <div className="quote">{data.quote}</div>
-        <div>
-          <img
-            className="xueImage"
-            src="/images/xue.png"
-            alt="xue wu zhi jing"
-          />
+  const LandingComponent = () => {
+    const contentRef = useRef(null);
+
+    useEffect(() => {
+      contentRef.current.scrollIntoView({ behavior: "smooth" });
+    }, []);
+
+    return (
+      <>
+        <video autoPlay muted loop id="nycVideo">
+          <source src="videos/nyc.mp4" type="video/mp4" />
+        </video>
+        <div ref={contentRef} className="content">
+          <div className="quote">{data.quote}</div>
+          <div className="xueImageContainer">
+            <img
+              className="xueImage"
+              src="/images/xue.png"
+              alt="xue wu zhi jing"
+            />
+          </div>
         </div>
-      </div>
-      <div className="content">
-        <div className="content-box">
-          <div className="centered-image">
+        <div className="content">
+          <div className="content-box">
+            <div className="centered-image">
+              <ScrollAnimation animateIn="fadeIn">
+                <div className="profile-image">
+                  <img src="/images/profile1.jpg" alt="profile" />
+                </div>
+              </ScrollAnimation>
+            </div>
+
             <ScrollAnimation animateIn="fadeIn">
-              <div className="profile-image">
-                <img src="/images/profile1.jpg" alt="profile" />
+              <div className="profile-text">
+                <h1>{data.title}</h1>
+                <p>{data.career}</p>
+                <div className="professional-box">
+                  <Link to="/professional" className="nav-link-button">
+                    Professional
+                  </Link>
+                  {/* link for Github */}
+                  <a
+                    href="https://github.com/iankhou"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="media-icon"
+                      src="https://img.icons8.com/color-glass/48/github.png"
+                      alt="github"
+                    />
+                  </a>
+                  {/* link for LinkedIn */}
+                  <a
+                    href="https://www.linkedin.com/in/ian-k-hou/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {/* image scaled to 48px */}
+                    <img
+                      className="media-icon"
+                      src="https://brand.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+                      alt="linkedin"
+                      width="48px"
+                    />
+                  </a>
+                </div>
+
+                <p>{data.personal}</p>
+                <Link to="/personality" className="nav-link-button">
+                  Personal
+                </Link>
               </div>
             </ScrollAnimation>
           </div>
-
-          <ScrollAnimation animateIn="fadeIn">
-            <div className="profile-text">
-              <h1>{data.title}</h1>
-              <p>{data.career}</p>
-              <div className="professional-box">
-                <Link to="/professional" className="nav-link-button">
-                  Professional
-                </Link>
-                {/* link for Github */}
-                <a
-                  href="https://github.com/iankhou"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    className="media-icon"
-                    src="https://img.icons8.com/color-glass/48/github.png"
-                    alt="github"
-                  />
-                </a>
-                {/* link for LinkedIn */}
-                <a
-                  href="https://www.linkedin.com/in/ian-k-hou/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {/* image scaled to 48px */}
-                  <img
-                    className="media-icon"
-                    src="https://brand.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
-                    alt="linkedin"
-                    width="48px"
-                  />
-                </a>
-              </div>
-
-              <p>{data.personal}</p>
-              <Link to="/personality" className="nav-link-button">
-                Personal
-              </Link>
-            </div>
-          </ScrollAnimation>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  };
 
   return isLoading ? <Loading /> : <LandingComponent />;
 };
